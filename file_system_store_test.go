e48fd452d2ecb61c5c7cae5696c01e4e6c72f3d2
Ryan Thompson <37919458+arrtee38@users.noreply.github.com>
2022-01-06 03:08:48 +0000

    Refactored portfolio read test

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -6,14 +6,12 @@ import (
 )
 
 func TestFileSystemStore(t *testing.T) {
-
-	t.Run("portfolio from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+	database := strings.NewReader(`[
             {"Name": "Stonks", "Amount": 30.0},
             {"Name": "Cryptos", "Amount": 70.0}]`)
-
-		store := FileSystemAssetStore{database}
-
+	store := FileSystemAssetStore{database}
+		
+	t.Run("portfolio from a reader", func(t *testing.T) {
 		got := store.GetPortfolio()
 
 		want := []Asset{
@@ -23,23 +21,20 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertPortfolio(t, got, want)
 
+		//check that re-read starts at beginning of file
 		got = store.GetPortfolio()
 		assertPortfolio(t, got, want)
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
-			{"Name": "Stonks", "Amount": 70.0},
-			{"Name": "Cryptos", "Amount": 30.0}]`)
-
-		store := FileSystemAssetStore{database}
-
 		got := store.GetAssetAmount("Cryptos")
-
-		want := 30.0
-
-		if got != want {
-			t.Errorf("got %f want %f", got, want)
-		}
+		assertAssetAmount(t, got, 70.0)
 	})
+}
+
+func assertAssetAmount(t testing.TB, got, want float64) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
 }
